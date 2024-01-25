6bc1d7405db9d0782e4627526384e6325205838e
Jorgespra <88691296+Jorgespra@users.noreply.github.com>
2024-01-24 21:33:26 -0600

    Add files via upload

---
+++ santander-java-testing-20232-quality/src/test/java/org/bedu/ApplicationTest.java
@@ -0,0 +1,14 @@
+package org.bedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	//Hace referencia a un Smog Test
+	@Test
+	void contextLoads() {
+	}
+
+}
